9e1f334b259243eaac82abfc6cafb14389a505c0
cyrusmith <alexander.sutyagin@gmail.com>
2014-01-13 10:00:06 +0600

    Added mask image

--- ImageCrop/src/ru/interosite/examples/imagecrop/MainActivity.java
+++ ImageCrop/src/ru/interosite/examples/imagecrop/MainActivity.java
@@ -1,15 +1,45 @@
 package ru.interosite.examples.imagecrop;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	private class LoadImagesTask extends AsyncTask<Void, Integer, Void>
+	{
+
+		@Override
+		protected Void doInBackground(Void... params)
+		{
+
+			return null;
+		}
+
+	}
+
+	private LoadImagesTask imagesTask = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	protected void onStart()
+	{
+		super.onStart();
+		imagesTask = new LoadImagesTask();
+		imagesTask.execute();
+	}
+
+	@Override
+	protected void onStop()
+	{
+		super.onStop();
+		imagesTask.cancel(true);
+	}
 }
